be3ed99e7f77cc1858527037a9262050b2e24aa1
hunhoffe <hunhoffe@users.noreply.github.com>
2026-01-09 16:02:07 +0000

    deploy: 9c3a745c2d4855d2cd7b760f4be2e9ac755d65d4

---
+++ python/html/search/functions_3.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['can_5fused_5fshared_5fmem_0',['can_used_shared_mem',['../classiron_1_1dataflow_1_1objectfifo_1_1ObjectFifo.html#a1375c3b7a192dcc58819524d9b6eb549',1,'iron::dataflow::objectfifo::ObjectFifo']]],
+  ['ceildiv_1',['ceildiv',['../namespacetaplib_1_1utils.html#a62af82f11dbafc603535bc7720e1e804',1,'taplib::utils']]],
+  ['check_5fdevice_5fconsistency_2',['check_device_consistency',['../classutils_1_1hostruntime_1_1hostruntime_1_1HostRuntime.html#a5d46e30d6bbe940dca6af309d0a113d2',1,'utils::hostruntime::hostruntime::HostRuntime']]],
+  ['cleanup_3',['cleanup',['../classutils_1_1hostruntime_1_1xrtruntime_1_1hostruntime_1_1CachedXRTRuntime.html#a9222185051ba97d298ed648fd7298c8d',1,'utils::hostruntime::xrtruntime::hostruntime::CachedXRTRuntime']]],
+  ['cols_4',['cols',['../classiron_1_1device_1_1device_1_1Device.html#a901d88d67355cd2e6dcf452da3a1e538',1,'iron::device::device::Device']]],
+  ['compare_5faccess_5forders_5',['compare_access_orders',['../classtaplib_1_1tap_1_1TensorAccessPattern.html#a451fffb21a434a3ce050ac8e47798f59',1,'taplib.tap.TensorAccessPattern.compare_access_orders()'],['../classtaplib_1_1tas_1_1TensorAccessSequence.html#a7d749f8e95c0a422298da4e5b2321095',1,'taplib.tas.TensorAccessSequence.compare_access_orders()']]],
+  ['cons_6',['cons',['../classiron_1_1dataflow_1_1objectfifo_1_1ObjectFifo.html#a9074a9e04b32080262f3755b0f24b36a',1,'iron::dataflow::objectfifo::ObjectFifo']]],
+  ['create_5fclass_7',['create_class',['../namespaceiron_1_1device_1_1device.html#a75a6d3df1229a191db361283e9b81b6d',1,'iron::device::device']]]
+];
---
+++ python/html/search/variables_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['kernel_0',['kernel',['../classutils_1_1hostruntime_1_1xrtruntime_1_1hostruntime_1_1XRTKernelHandle.html#a3c80ff50b8ff78ca076b38630c90f69a',1,'utils::hostruntime::xrtruntime::hostruntime::XRTKernelHandle']]]
+];
